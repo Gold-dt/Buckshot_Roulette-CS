cb307b4487a50ba26c36e3f51c6cc2eb0eedd035
Gold dt <82865840+Gold-dt@users.noreply.github.com>
2024-10-26 14:06:25 +0200

    test

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,13 +1,33 @@
-﻿using GameLib;
+﻿using System;
+using GameLib;
 
-
-List<string> test = new List<string>()
+namespace Game
 {
-    "Gold_dt;21;1;1;Kö:2","Enigma;1;2;2;Fö:102,Fa:32"
-};
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-Engine engine = new(test);
-engine.ProfileName = "Gold_dt";
+            List<string> test = new List<string>()
+            {
+                "Gold_dt;21;1;1;Kö:2","Enigma;1;2;2;Fö:102,Fa:32"
+            };
+
+            Engine engine = new(test);
+            Functions functions = new();
+            engine.ProfileName = "Gold_dt";
 
 
-Console.WriteLine(engine);
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+            functions.DisplayIntro(engine.Tittle, "\t\t");
+            
+            
+
+
+
+
+        }
+    }
+}
